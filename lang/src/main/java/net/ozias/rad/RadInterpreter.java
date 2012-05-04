c02b117d0cc2ff2f654a7599a35f8f9dd98482ff
Jason Ozias <jozias@eyemedvisioncare.com>
2012-05-04 13:31:47 -0400

    Updated to use jline as the input handler.

--- lang/src/main/java/net/ozias/rad/RadInterpreter.java
+++ lang/src/main/java/net/ozias/rad/RadInterpreter.java
@@ -3,15 +3,14 @@
  */
 package net.ozias.rad;
 
+import jline.console.ConsoleReader;
+
 import net.ozias.rad.lang.Parser;
 import net.ozias.rad.lang.eval.Statement;
 
 import org.apache.log4j.Logger;
 
-import java.io.Console;
-import java.io.PrintWriter;
-
-import java.util.Scanner;
+import java.io.IOException;
 
 /**
  * R@d Command Interpreter.
@@ -29,77 +28,33 @@ public class RadInterpreter implements Runnable {
    * @see  java.lang.Runnable#run()
    */
   @Override public void run() {
-    final Console con = System.console();
 
-    if ( con == null ) {
-      LOG.error( "Unable to access system console." );
-    } else {
-      final PrintWriter writer = con.writer();
-      final RadPrompt prompt = new RadPrompt( con.writer() );
-      final Scanner scanner = new Scanner( con.reader() );
-      scanner.useDelimiter( System.getProperty( "line.separator" ) );
+    try {
+      final ConsoleReader con = new ConsoleReader();
+      con.setPrompt( "r@d: " );
+      con.println( "Welcome to the R@d Intepreter!" );
+      con.println( "Enter help to get started." );
 
-      writer.println( "Welcome to the R@d Intepreter!" );
-      writer.println( "Enter help to get started." );
-      prompt.print();
+      String token = con.readLine();
 
-      while ( scanner.hasNext() ) {
-        final String token = scanner.next();
+      while ( token != null ) {
+        token = token.trim();
 
-        if ( token == null ) {
-          writer.println( "Null token." );
-          prompt.print();
-        } else if ( token.isEmpty() ) {
-          writer.println( "Empty token." );
-          prompt.print();
+        if ( token.isEmpty() ) {
+          con.println( "Empty token." );
         } else if ( token.equalsIgnoreCase( "exit" ) ) {
-          writer.println( "Exiting.  This may take a few moments while everything cleans up." );
+          con.println( "Exiting.  This may take a few moments while everything cleans up." );
 
           break;
         } else {
-          writer.println( Statement.eval( Parser.parseStatement( token ) ) );
-          prompt.print();
+          con.println( Statement.eval( Parser.parseStatement( token ) ) );
         }
+        con.flush();
+        token = con.readLine();
       }
-    }
-  }
-
-  //~ Inner Classes --------------------------------------------------------------------------------------------------------------------------------------------
-
-  /**
-   * Generate the Rad prompt.
-   */
-  private class RadPrompt {
-
-    //~ Static fields/initializers -----------------------------------------------------------------------------------------------------------------------------
-
-    /** The prompt value. */
-    private static final String PROMPT = "r@d: ";
-
-    //~ Instance fields ----------------------------------------------------------------------------------------------------------------------------------------
-
-    /** The writer associated with the console. */
-    private final transient PrintWriter writer;
-
-    //~ Constructors -------------------------------------------------------------------------------------------------------------------------------------------
-
-    /**
-     * Creates a new RadPrompt object.
-     *
-     * @param  writer  The writer associated with the console.
-     */
-    public RadPrompt( final PrintWriter writer ) {
-      this.writer = writer;
-    }
-
-    //~ Methods ------------------------------------------------------------------------------------------------------------------------------------------------
-
-    /**
-     * Spit out the prompt.
-     */
-    public void print() {
-      writer.print( PROMPT );
-      writer.flush();
+      con.flush();
+    } catch ( IOException e ) {
+      LOG.error( "Error creating console reader.", e );
     }
   }
 }
